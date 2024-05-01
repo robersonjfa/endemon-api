66b4fbaf45068d4d03eb6625cbff369cab11610b
Roberson Alves <robersonjfa@gmail.com>
2024-05-01 14:41:55 -0300

    Ajuste na estrutura do projeto

--- src/index.js
+++ src/index.js
@@ -36,13 +36,13 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
-  );
+// app.use(function (req, res, next) {
+//   res.setHeader(
+//     'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+//   );
   
-  next();
-});
+//   next();
+// });
 
 //configure the app.
 app.get("/", (req, res) => {
